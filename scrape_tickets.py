ca725e027d402e3ff7c45f9f9ffe73d4056397a4
vincentlannelongue <56261386+vincentlannelongue@users.noreply.github.com>
2022-10-07 13:57:16 +0200

    Arthur's implementation

--- scrape_tickets.py
+++ scrape_tickets.py
@@ -1,41 +1,42 @@
-##### TO DO #####
-# Implement selenium explicit wait
-# Extract category number
-
-
 from cgitb import text
-from datetime import date
 import time
 import csv
+from datetime import date
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+options = Options()
+options.headless = False
 
-import csv 
-
-extract_date = date.today()
-waiting_time = 10
+today = date.today()
 
 driver = webdriver.Chrome(ChromeDriverManager().install()) #always re-install chromedriver because I couldn't make it work with a specified path
+driver = driver
 html_page = driver.get("https://billetterie.psg.fr/fr/second/match-foot-masculin-paris-vs-marseille/")
 
 #Accept cookies
-
-cookie = WebDriverWait(driver, waiting_time).until(
+cookie = WebDriverWait(driver, 5).until(
     EC.element_to_be_clickable(("xpath", '//*[@id="didomi-notice-agree-button"]'))
 )
 cookie.click()
 
-
 #Close pop-up
-fast_popup = WebDriverWait(driver, waiting_time).until(
+fast_popup = WebDriverWait(driver, 5).until(
     EC.element_to_be_clickable(("xpath", '//div[@class="stack:32px psgTicketplaceFastest wrapper:450px fs:14px ta:center"]//descendant::button'))
 )
 fast_popup.click()
+
+# nl_popup = WebDriverWait(driver, 10).until(
+#     EC.element_to_be_clickable(("xpath", '//a[@class="wp-optin-dialog-button wp-optin-dialog-negativeButton"]'))
+# )
+# nl_popup.click()
+# time.sleep(1)
 
 # Object fetching 
 booking_cats = driver.find_elements("xpath", '//div[@class="cardBox relative cardBoxHover bookingCategory"]')
@@ -46,61 +47,62 @@ booking_accesses = driver.find_elements("xpath", '//button[@class="flex ai:cente
 Tickets = []
 # the loop sometimes breaks after a few categories 
 # probably a specific access or ticket badly handled in a cat after the 2nd one
-for i, booking_cat in enumerate(booking_cats[:2]):
+for i, booking_cat in enumerate(booking_cats):
 
     cat_accesses = []
     print(booking_cat.text)
+    booking_cats_buttons[i] = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((booking_cats_buttons[i]))
+        )
     booking_cats_buttons[i].click()  # opening category 
     cat_number = i # not the real number, needs to be implemented
 
     # Loop to get the access buttons specific to this category
     for j, access_button in enumerate(booking_accesses):
         if access_button.aria_role != 'none':
-            WebDriverWait(driver, waiting_time).until(
-                EC.element_to_be_clickable(access_button)
-            )
             cat_accesses.append(access_button)
 
     # Loop on accesses
     for access in cat_accesses:
-        WebDriverWait(driver, waiting_time).until(
-                EC.element_to_be_clickable(access)
-            )
+        access = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((access))
+        )
         access.click()
         # Loop on tickets
-        rangs = WebDriverWait(driver, waiting_time).until(
-            EC.presence_of_all_elements_located(("xpath", '//button[@class="flex ai:center py:10px w:100% fw:700 bookingResaleToggle dropdownArrows relative cardBoxMinimalContent"]//descendant::span[3]'))
-        )
+        access_name = driver.find_element("xpath", '//span[@class="block"]')
+        cat_name = driver.find_element("xpath", '//b[@class="flex flexcolumn w:fit-content tt:capitalize bookingCategoryName"]')
+        rangs = driver.find_elements("xpath", '//button[@class="flex ai:center py:10px w:100% fw:700 bookingResaleToggle dropdownArrows relative cardBoxMinimalContent"]//descendant::span[3]')
+        quantities = driver.find_elements("xpath", '//button[@class="flex ai:center py:10px w:100% fw:700 bookingResaleToggle dropdownArrows relative cardBoxMinimalContent"]//descendant::span[6]')
+        prices = driver.find_elements("xpath", '//button[@class="flex ai:center py:10px w:100% fw:700 bookingResaleToggle dropdownArrows relative cardBoxMinimalContent"]//descendant::span[9]')
+        cat_name = cat_name.text
+        access_name = access_name.text
 
-        quantities = WebDriverWait(driver, waiting_time).until(
-            EC.presence_of_all_elements_located(("xpath", '//button[@class="flex ai:center py:10px w:100% fw:700 bookingResaleToggle dropdownArrows relative cardBoxMinimalContent"]//descendant::span[6]'))
-        )
-        prices = WebDriverWait(driver, waiting_time).until(
-            EC.presence_of_all_elements_located(("xpath", '//button[@class="flex ai:center py:10px w:100% fw:700 bookingResaleToggle dropdownArrows relative cardBoxMinimalContent"]//descendant::span[9]'))
-        )
         for k in range(len(rangs)):
             try:    # the exception doesn't seem to pick up the error from selenium
-                Tickets.append([cat_number, extract_date, rangs[k].text, quantities[k].text, prices[k].text])
+                Tickets.append([today, cat_number, cat_name, access_name, rangs[k].text, quantities[k].text, prices[k].text])
             except Exception as e:
                 print("invalid ticket")
-                raise(e)
-
-        return_button = driver.find_element("xpath", '//div[@class="flex bookingToolbar"]//descendant::button')
+        time.sleep(1)        
+        return_button = driver.find_element("xpath", '//button[@title="Retour"]')
         return_button.click()
-        WebDriverWait(driver, waiting_time).until(
-            EC.element_to_be_clickable(booking_cats_buttons[i])
+
+        booking_cats_buttons[i] = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((booking_cats_buttons[i]))
         )
         booking_cats_buttons[i].click()  # reopening category to get the next access
 
-
+    booking_cats_buttons[i] = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((booking_cats_buttons[i]))
+        )
     booking_cats_buttons[i].click()  # closing category 
+
 
 # creating the csv
 
-fields = ['Cat', 'Extraction_date', 'Rank', 'Quant', 'Price'] 
+fields = ['Date', 'Cat_index', 'Cat_name', 'Access', 'Rank', 'Quant', 'Price'] 
 rows = Tickets
     
-filename = f"tickets_{extract_date}.csv"
+filename = "db_psg_price_" + str(today)
     
 # writing to csv file 
 with open(filename, 'w', encoding="utf-8") as csvfile: 
@@ -108,5 +110,4 @@ with open(filename, 'w', encoding="utf-8") as csvfile:
     csvwriter.writerow(fields) 
     csvwriter.writerows(rows)
 
-driver.quit()
 print("done")
